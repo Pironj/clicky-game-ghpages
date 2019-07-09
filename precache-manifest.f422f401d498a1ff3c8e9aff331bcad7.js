850e567128c6b8b5b7d74e8e7e7ecff53b07fbd4
Julian Piron <pironj27@gmail.com>
2019-07-08 21:36:06 -0700

    Updates

---
+++ precache-manifest.f422f401d498a1ff3c8e9aff331bcad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be0fcb47c6fe17dc97b1f4ebb93aef6",
+    "url": "/clicky-game-ghpages/index.html"
+  },
+  {
+    "revision": "596d41e3b55d5ec77012",
+    "url": "/clicky-game-ghpages/static/css/main.dcd33d33.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game-ghpages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "596d41e3b55d5ec77012",
+    "url": "/clicky-game-ghpages/static/js/main.39fc1f09.chunk.js"
+  },
+  {
+    "revision": "5cd116205a21957f54bf",
+    "url": "/clicky-game-ghpages/static/js/runtime~main.b6c50e20.js"
+  }
+]);
