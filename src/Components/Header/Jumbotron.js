c5138faff48feef88d423f186b24bcb914cd1bea
Julian Piron <pironj27@gmail.com>
2019-07-10 17:59:13 -0700

    updated mobile header responsiveness

--- src/Components/Header/Jumbotron.js
+++ src/Components/Header/Jumbotron.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Header= () => {
   return (
     <nav className="jumbotron jumbotron-fluid">
-      <div className="container">
+      <div className="container headerText">
         <h1 className="display-4">Clicky Memory Game</h1>
         <p className="lead">Click on the images below to earn points. If you click on the same image twice the game will end.</p>
       </div>
